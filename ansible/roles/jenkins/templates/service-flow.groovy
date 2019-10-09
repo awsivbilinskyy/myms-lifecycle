8000437ea3fd81e771b35140625ca8ba8fa05f04
Illia Bilinskyi <illia_bilinskyi@epam.com>
2019-10-09 16:51:47 +0300

    chapter Continuous Integration updated code

--- ansible/roles/jenkins/templates/service-flow.groovy
+++ ansible/roles/jenkins/templates/service-flow.groovy
@@ -1,10 +1,11 @@
 node("cd") {
     git url: "https://github.com/awsivbilinskyy/my${serviceName}.git"
     def flow = load "/data/scripts/workflow-util.groovy"
+    def prodIp = "10.100.198.201"
     flow.provision("prod2.yml")
     flow.buildTests(serviceName, registryIpPort)
     flow.runTests(serviceName, "tests", "")
     flow.buildService(serviceName, registryIpPort)
     flow.deploy(serviceName, prodIp)
-    flow.updateProxy(serviceName, "prod")
+    flow.updateProxy(serviceName, prodIp, "prod")
 }
